6710bbe2991b393694c669d53c3ebbc970634504
Nishikant2003 <Nishikant.Raut@zeuslearning.com>
2025-06-03 19:20:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ function valid(){
     const name=document.forms["myForm"]["name"].value;
     if(name==""){
         alert("Name field is empty")
+        document.getElementById("name").focus();
         return false;
     }
     const comments=document.forms["myForm"]["comments"].value;
     if(comments==""){
         alert("comments field is empty")
+        document.getElementById("comments").focus();
         return false;
     }
     const gender=document.forms["myForm"]["gender"].value;
     if(gender==""){
         alert("gender is not chosen")
+        document.getElementById("gender").focus();
         return false;
     }
 }
